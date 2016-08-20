46c7785cac810316d0e7430a4a13f85f790c212e
KrishnaChaitanya <chait.amjuri@gmail.com>
2016-08-20 21:22:06 +0530

    Chapter 8

---
+++ .idea/javascriptTutorial_08.js
@@ -0,0 +1,50 @@
+/**
+ * Created by krishnachaitanyaamjuri on 20/08/16.
+ */
+
+/*
+ Normally, when you forget to put var in front of your variable, as with counter in the example,
+ JavaScript quietly creates a global variable and uses that.
+ In strict mode, however, an error is reported instead. This is very helpful
+ */
+// Strict Mode
+
+'use strict';
+
+function canYouSpotTheProblem() {
+    for(var counter = 0; counter < 10; counter++) {
+        console.log('Happy');
+    }
+}
+
+canYouSpotTheProblem();
+
+function Person(name) {
+    this.name = name;
+}
+var ferdino = new Person('babe');
+
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
+Vector.prototype.plus = function (other) {
+    return new Vector(this.x + other.x, this.y + other.y);
+}
+
+// testing - use testing frameworks for this
+
+// exception
+
+function promptDirection(question) {
+    var result = question;
+    if(result.toLowerCase() == "left") return "L";
+    if(result.toUpperCase() == "right") return "R";
+    throw new Error("Invalid direction: " + result);
+}
+
+try {
+    console.log(promptDirection("which Way?"));
+} catch (error) {
+    console.log(error);
+}
